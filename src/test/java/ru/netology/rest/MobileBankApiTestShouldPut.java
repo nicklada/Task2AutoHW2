5e3b50860b841610471405ba013e06332f525524
lada <nicklada94@gmail.com>
2020-06-01 11:50:51 +0300

    Test with Put added

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestShouldPut.java
@@ -0,0 +1,21 @@
+package ru.netology.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class MobileBankApiTestShouldPut {
+
+    @Test
+    void shouldPut() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Petrov")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(12345));
+    }
+}
